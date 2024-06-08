8a22673e22cacefab852df55ffe7a7916a8c553b
nur <nurchowdhury56@gmail.com>
2024-06-09 00:02:14 +0600

    update

--- public/src/App.js
+++ public/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Register />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/chat' element={<Chat />} />
